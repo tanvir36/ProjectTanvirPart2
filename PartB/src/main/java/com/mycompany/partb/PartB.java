513539e5001682247f3e5f9e1d9b2cfaab340114
tanvi <tanvi@10.0.0.8>
2026-06-01 22:48:18 -0400

    Added some print statements in the main class

---
+++ PartB/src/main/java/com/mycompany/partb/PartB.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.partb;
+
+/**
+ *
+ * @author tanvi
+ */
+public class PartB {
+
+    public static void main(String[] args) {
+       System.out.println("Project developed using NetBeans IDE");
+       System.out.println("Version Control: Git & GitHub");
+       System.out.println("Current workflow: feature -> staging -> main");
+       
+    }
+}
